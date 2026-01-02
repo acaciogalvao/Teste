2f71e2a95ac7718b116983bbf89a99a33b80da70
Acácio Galvão <60447856+acaciogalvao@users.noreply.github.com>
2026-01-02 17:51:57 -0300

    refactor: Remove React and Tailwind CSS dependencies
    The application is being refactored to use vanilla JavaScript. This commit removes React and its related dependencies, as well as the Tailwind CSS CDN link, as styling is now handled directly in HTML. The entry point is updated to be a plain JavaScript file.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Vanilla JavaScript entry point
+console.log('Nenê Teste application loaded.');
